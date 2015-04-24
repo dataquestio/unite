3291a343c3ac0159d9a422839950cadb78d2aa3f
Vik Paruchuri <vik.paruchuri@gmail.com>
2015-04-24 14:47:48 -0400

    Fix up files

--- settings.py
+++ settings.py
@@ -6,11 +6,11 @@ DATA_DIR = os.path.join(BASE_DIR, "data")
 # This file was removed before pushing to github.
 RAW_DATA_FILE = os.path.join(DATA_DIR, "clean_cars.csv")
 
-TRAIN_DATA_FILE = os.path.join(DATA_DIR, "clean_cars_train.csv")
-VALID_DATA_FILE = os.path.join(DATA_DIR, "clean_cars_valid.csv")
+TRAIN_DATA_FILE = os.path.join(DATA_DIR, "train.csv")
+VALID_DATA_FILE = os.path.join(DATA_DIR, "valid.csv")
 
 # This file was removed before pushing to github.
-TEST_DATA_FILE = os.path.join(DATA_DIR, "clean_cars_test.csv")
+TEST_DATA_FILE = os.path.join(DATA_DIR, "test.csv")
 
 # The column to make predictions for.
 PREDICTION_COLUMN = "GHG"
